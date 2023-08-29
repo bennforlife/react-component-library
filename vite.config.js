2ecb9bf2a8d6df61c1b160b0098d2f73d8ee3078
bennforlife <bennforlife@users.noreply.github.com>
2023-08-29 17:49:11 -0400

    Install linaria

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import linaria from "@linaria/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), linaria()],
+});
