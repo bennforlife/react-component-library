3a8fe37b9484860abb89b93d61ec31204aa8fa48
bennforlife <bennforlife@users.noreply.github.com>
2023-08-30 21:27:58 -0400

    Configure linaria to compile css from jsx files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import linaria from '@linaria/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), linaria()],
+  plugins: [
+    react(),
+    linaria({
+      include: ['**/*.{js,jsx}'],
+    }),
+  ],
 })
