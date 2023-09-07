8a109cd74411ecc5538c531d4d658ac3c8863e15
bennforlife <bennforlife@users.noreply.github.com>
2023-09-07 16:40:59 -0400

    Create global css styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { globals } from './global.styles.js'
 import App from './App.jsx'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
